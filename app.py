4bb0c1566cce3e209db3d3bce85d8ab92cfa04dd
Alan Liu <gn02355346yi@hotmail.com>
2018-07-25 17:47:14 +0800

    Add code

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def handle_message(event):
     profile = line_bot_api.get_profile(ID)
     if any(x in message for x in ["地點", "哪裡"]):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "地點在九樓喔！"))
-    elif any(x in message for x in ["時間", "幾點"]):
+    elif any(x in message for x in ["時間", "幾點", "時候"]):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "時間是 8/10 下午四點喔！"))    
     elif any(x in message for x in ["你是誰", "你叫什麼", "名字"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "你好~ 我是帥哥道儒 想跟我說話的話要先加我好友喔！"),StickerSendMessage(package_id = '3', sticker_id = '124')] )      
@@ -52,7 +52,8 @@ def handle_message(event):
     elif any(x in message for x in ["要幹嘛", "內容", "活動"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "吃吃喝喝有吃有拿喔！ 還有Intern們拍的精彩影片～"),StickerSendMessage(package_id = '3', sticker_id = '134')])
 
-
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = message + "你大頭啦"))         
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
