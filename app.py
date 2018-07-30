0f0301fb0a6bb2935622c32d6823744944934282
Alan Liu <gn02355346yi@hotmail.com>
2018-07-30 16:20:09 +0800

    Add Code

--- app.py
+++ app.py
@@ -37,27 +37,36 @@ def handle_message(event):
     message = event.message.text
     ID = event.source.user_id
     profile = line_bot_api.get_profile(ID)
-    if any(x in message for x in ["地點", "哪裡"]):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "地點在九樓喔！"))
+    if any(x in message for x in ["地點", "哪裡", "住哪"]):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "在九樓喔！"))
+
     elif any(x in message for x in ["時間", "幾點", "時候"]):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "時間是 8/10 中午12點喔！"))    
+    
     elif any(x in message for x in ["你是誰", "你叫什麼", "名字"]):
-        line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "你好~ 我是帥哥道儒 想跟我說話的話要先加我好友喔！"),StickerSendMessage(package_id = '3', sticker_id = '124')] )      
+        line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "你好~ 我是蛋糕 想跟我說話的話要先加我好友喔！"),StickerSendMessage(package_id = '3', sticker_id = '124')] )      
+    
     elif any(x in message for x in ["Ines", "ines", "白庭安"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "Ines在拍孤兒怨續集喔！"),ImageSendMessage(original_content_url='https://i.imgur.com/lIZYmaX.png',
     preview_image_url='https://i.imgur.com/lIZYmaX.png')])
+    
     elif any(x in message for x in ["Daniel", "daniel", "郭道儒"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "Daniel在賣手錶喔！"),ImageSendMessage(original_content_url='https://cc.tvbs.com.tw/news3.0/tvbs/news/entertainment/images/2015/11/06/2.jpg?__scale=h:441,w:662,cx:3,cy:0,ch:437,cw:659',
     preview_image_url='https://cc.tvbs.com.tw/news3.0/tvbs/news/entertainment/images/2015/11/06/2.jpg?__scale=h:441,w:662,cx:3,cy:0,ch:437,cw:659')])
+    
     elif any(x in message for x in ["Lawrence", "lawrence", "羅倫斯"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "Lawrence在佛羅倫斯喔"),ImageSendMessage(original_content_url='https://img.appledaily.com.tw/images/ReNews/20150622/640_a31496282a31c58a46c19028b9a28ed7.jpg',
     preview_image_url='https://img.appledaily.com.tw/images/ReNews/20150622/640_a31496282a31c58a46c19028b9a28ed7.jpg')])    
-    elif any(x in message for x in ["你好", "妳好", "嗨"]):
+    
+    elif any(x in message for x in ["你好", "妳好", "嗨", "安安"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = profile.display_name + "你好!" + " 要來參加 Appreciation Day 活動喔～"),StickerSendMessage(package_id = '3', sticker_id = '134')])
+    
     elif any(x in message for x in ["來幹嘛", "目的", "做什麼", "做啥"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = profile.display_name + "你好!" + " 我是來宣傳 Appreciation Day 活動的喔～"),StickerSendMessage(package_id = '3', sticker_id = '134')])
+    
     elif any(x in message for x in ["要幹嘛", "內容", "活動"]):
         line_bot_api.reply_message(event.reply_token, [TextSendMessage(text = "吃吃喝喝有吃有拿喔！ 還有Intern們拍的精彩影片～"),StickerSendMessage(package_id = '3', sticker_id = '134')])
+    
     elif message in gag:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = gag[message])) 
  
